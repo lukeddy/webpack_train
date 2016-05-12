44a152fc5c8769da62fce6e279d985c35176fd5b
tzq668766 <tzq618766@gmail.com>
2016-05-12 19:24:01 +0800

    添加hello webpack示例

---
+++ webpack1/README.md
@@ -0,0 +1,10 @@
+#webpack示例1
+
+##资源打包
+`webpack`  
+
+##启动服务
+`npm start`
+
+##浏览器中打开测试
+`http://localhost:8080`
---
+++ webpack1/index.js
@@ -0,0 +1,7 @@
+require('./index.css');
+
+
+console.log("你好webpack,我是日志信息！");
+var eleMain=document.getElementById('main'); 
+eleMain.innerHTML='Webpack 你好!!!!!'; 
+
